0014f685f11e557e821b6d3b819a8e462540bcf2
19nikilangiu91 <niki.langiu@gmail.com>
2023-01-24 15:04:11 +0100

    Created class Movie

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php-Oop-1</title>
+</head>
+
+<body>
+    <?php
+    // Definiamo una classe "Movie".
+    class Movie
+    {
+        // all'interno della classe dichiariamo delle "variabili d'istanza"
+        public $title;
+        public $actors;
+        public $director;
+        public $genre;
+        public $year;
+    }
+    ?>
+
+</body>
+
+</html>
